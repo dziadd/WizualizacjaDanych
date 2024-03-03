eace67dbbf030364cbbe77ccd86238b57e413b83
dziadd <147257891+dziadd@users.noreply.github.com>
2024-03-03 13:31:30 +0100

    1

--- main.py
+++ main.py
@@ -1,16 +1,84 @@
-# This is a sample Python script.
+# a = 56
+# print(a)
+# print(type(a))
+# b = 5.5
+# print(b)
+# print(type(b))
+# zmienna_tekstowa = 'Wizualizacja danych'
+# print(zmienna_tekstowa)
+# print(type(zmienna_tekstowa))
+#
+# a = 6
+# b = 3
+#
+# c = a + b
+# print(c)
+# d = a - b
+# print(d)
+# e = 4
+# f = b//a
+# print(f)
+# f = a//b
+# print(f)
+# g = a ** 2
+# print(g)
+# h = pow(a, 2)
+# print(h)
+#
+# i = 6 ** (1/2)
+# j = pow(6, 1/2)
+# print(i)
+# print(j)
+#
+# k = 'Wizualizacja danych'
+# l = ' grupa 1'
+# m = 1
+# print(k+l)
+# print('liczba a jest rowna {:.3f}, liczba b jest rowna {}'
+#       .format(a, b))
+#
+# a=input('Wprowadz liczbe: ')
+# print (a)
+# print(type(a))
+# print(a*5)
+# print(type(a))
+#
+# import sys
+#
+# sys.stdout.write('Wprowadz liczbe: ')
+# b = sys.stdin.readline()
+# print(b)
+# print(type(b))
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# lista = [5, 6.6, 34, 'a', 'b', [2, 3,4], 'ab']
+# print(lista)
+# lista.append(67)
+# print(lista)
+# lista.insert(2, 'c')
+# print(lista)
+# lista.extend([20, 21, 22])
+# print(lista)
+# lista.pop()
+# print(lista)
+# lista.pop(2)
+# print(lista)
+# lista.remove([2,3,4])
+# print(lista)
+# del lista[1]
+# print(lista)
+# lista.reverse()
+# print(lista)
+# lista.sort()  #tylko liczby nie mozna sortowac stringow z intami
+# print(lista)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+slownik = {'klucz': 'wartosc', 1: 2, 'a':5, 4: 'b'}
+print(slownik)
+print(slownik[4])
+slownik[6]=45
+print(slownik)
+slownik.pop(1)
+print(slownik)
+print(slownik.keys())
+print(slownik.values())
+del slownik[6]
+print(slownik)
